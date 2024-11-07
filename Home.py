2c59340156fe95b1baad6aaaba399ce049d33180
Jay Prakash <59421807+jayprakash97@users.noreply.github.com>
2024-11-07 06:05:00 -0800

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="WonderScribe", page_icon="📖", layout="wide")
+
+st.title("Welcome to WonderScribe")
+st.write("This is the main page of our multi-page app.")
+st.sidebar.title("📚 Table of Contents")
