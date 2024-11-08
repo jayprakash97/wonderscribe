a7e3e1b0df17333dc53e29efec5ef1ef181e1135
Avinash Kulkarni <40263379+kingKulkarni@users.noreply.github.com>
2024-11-08 19:03:05 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="WonderScribe", page_icon="📖", layout="wide")
 
+st.image("pages/WS_Logo.png", width=150)
 st.title("Welcome to WonderScribe")
 st.write("This is the main page of our multi-page app.")
 #st.sidebar.title("📚 Table of Contents")
