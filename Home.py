507fb1d3a3618a0e458b5237f50dd0dd2f53d79b
Avinash Kulkarni <40263379+kingKulkarni@users.noreply.github.com>
2024-11-08 19:04:21 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,5 +4,16 @@ st.set_page_config(page_title="WonderScribe", page_icon="📖", layout="wide")
 
 st.image("pages/WS_Logo.png", width=150)
 st.title("Welcome to WonderScribe")
-st.write("This is the main page of our multi-page app.")
+st.write(
+"""
+At WonderScribe, we use cutting-edge technology, including AI and advanced language models, to create a unique
+storytelling experience. Our platform allows kids to become co-authors of their adventures, customizing tales
+to reflect their dreams, personalities, and imaginations.
+
+We aim to make reading fun, interactive, and accessible to all children, no matter where they are. Through our
+innovative platform, we hope to foster a love of reading, spark creativity, and encourage every child to believe 
+in the magic of their own stories
+
+Join us on this exciting journey and watch your child's imagination soar!
+""")
 #st.sidebar.title("📚 Table of Contents")
