897b245010b0c7637f404cd95718979dde60f466
Avinash Kulkarni <40263379+kingKulkarni@users.noreply.github.com>
2024-11-13 19:18:48 +0530

    Update Evaluation_ak.py

--- pages/Evaluation_ak.py
+++ pages/Evaluation_ak.py
@@ -24,11 +24,8 @@ def bert_scores(predicated_response, gold_response):
         bert_scores['recall'] = sum(bert_scores['recall']) / len(bert_scores['recall'])
         bert_scores['f1'] = sum(bert_scores['f1']) / len(bert_scores['f1'])
         return bert_scores
-
-#@st.cache_data 
-def fetch_story_data(payload, _force_refresh=False):
-    # if _force_refresh:
-    #     st.cache_data.clear()
+ 
+def fetch_story_data(payload):
     AWS_API_URL = "https://wacnqhon34.execute-api.us-east-1.amazonaws.com/dev/"
     headers = {
         "Content-Type": "application/json"
@@ -44,9 +41,6 @@ def fetch_story_data(payload, _force_refresh=False):
         return []
 
 def main():
-    # if 'cache_cleared' not in st.session_state:
-    #     st.session_state.cache_cleared = False
-        
     validation_query_response_list = {
         0: {'query': 'Female,Srila,children,Magical Kingdoms,Fantacy,Brushing the tooth,Develop hygiene habbits,300',
             'gold_response': 'In the enchanting Magical Kingdoms, where unicorns roamed and fairies danced, lived a young girl named Srila. Srilas days were filled with wonder and adventure, but there was one task she often neglected - brushing her teeth. One morning, as Srila was exploring the enchanted forest, she stumbled upon a curious creature. It was a tiny tooth fairy, with delicate wings and a sparkling wand. The tooth fairy fluttered around Srila, sprinkling a magical dust on her teeth. "What is this?" Srila asked, feeling a tingle in her mouth. "This is a special tooth-cleaning dust," the fairy explained. "It will keep your teeth strong and shiny, but you must remember to brush them every day."Srila promised to be more diligent about her dental hygiene. The tooth fairy smiled and vanished in a shower of glitter. From that day on, Srila made brushing her teeth a daily ritual. She used a soft-bristled brush and sweet-tasting toothpaste infused with fairy magic. Srila would sing songs and dance as she brushed, making it a fun and enjoyable activity. As Srilas teeth grew healthier and brighter, she noticed other changes too. Her breath stayed fresh, and she could enjoy all her favorite foods without any discomfort. The tooth fairy would visit occasionally, praising Srilas dedication and rewarding her with small treasures. '
