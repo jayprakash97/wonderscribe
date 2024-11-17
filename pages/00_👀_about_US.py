2fbb5a8efbe9682d7c9b46ea42a1477ac802132d
Natsnet Demoz <25876006+Natsnet@users.noreply.github.com>
2024-11-17 11:13:26 -0800

    Update 00_👀_about_US.py

--- pages/00_👀_about_US.py
+++ pages/00_👀_about_US.py
@@ -49,9 +49,9 @@ st.markdown(
         everywhere. We believe in the power of stories to transport children to magical worlds, introduce them to 
         fascinating characters, and inspire endless possibilities.</p>
         
-       <p>At WonderScribe, we use cutting-edge technology, including AI and advanced language models, to create a unique
+       At WonderScribe, we use cutting-edge technology, including AI and advanced language models, to create a unique
         storytelling experience. Our platform allows kids to become co-authors of their adventures, customizing tales
-        to reflect their dreams, personalities, and imaginations.</p>
+        to reflect their dreams, personalities, and imaginations.
         
         We aim to make reading fun, interactive, and accessible to all children, no matter where they are. Through our
         innovative platform, we hope to foster a love of reading, spark creativity, and encourage every child to believe 
