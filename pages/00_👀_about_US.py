dbb77d8589b4431ebaacd5591082fcad0449ba2d
Natsnet Demoz <25876006+Natsnet@users.noreply.github.com>
2024-11-17 10:35:32 -0800

    Update 00_👀_about_US.py

--- pages/00_👀_about_US.py
+++ pages/00_👀_about_US.py
@@ -36,10 +36,6 @@ page_bg = """
 # Apply the custom CSS
 st.markdown(page_bg, unsafe_allow_html=True)
 
-# Streamlit content
-st.title("Welcome to My Streamlit App!")
-st.write("Enjoy the vibrant gradient background!")
-
  # +++++++
                               
 st.image("pages/images/WonderScribeLogo.png", width=150)
@@ -50,21 +46,20 @@ st.write(
 st.markdown(
     """
 <div class="custom-box">
-We are a passionate team of authors, educators, and creatives on a mission to ignite young readers' imaginations
-everywhere. We believe in the power of stories to transport children to magical words, introduce them to 
-fascinating characters, and inspire endless possibilities.
-
-At WonderScribe, we use cutting-edge technology, including AI and advanced language models, to create a unique
-storytelling experience. Our platform allows kids to become co-authors of their adventures, customizing tales
-to reflect their dreams, personalities, and imaginations.
-
-We aim to make reading fun, interactive, and accessible to all children, no matter where they are. Through our
-innovative platform, we hope to foster a love of reading, spark creativity, and encourage every child to believe 
-in the magic of their own stories
-
-Join us on this exciting journey and watch your child's imagination soar!
-"""
-    </div>
-    """,
+    <p> We are a passionate team of authors, educators, and creatives on a mission to ignite young readers' imaginations
+    everywhere. We believe in the power of stories to transport children to magical words, introduce them to 
+    fascinating characters, and inspire endless possibilities.
+    
+    At WonderScribe, we use cutting-edge technology, including AI and advanced language models, to create a unique
+    storytelling experience. Our platform allows kids to become co-authors of their adventures, customizing tales
+    to reflect their dreams, personalities, and imaginations.
+    
+    We aim to make reading fun, interactive, and accessible to all children, no matter where they are. Through our
+    innovative platform, we hope to foster a love of reading, spark creativity, and encourage every child to believe 
+    in the magic of their own stories
+    
+    Join us on this exciting journey and watch your child's imagination soar! </p> 
+</div>
+""",
     unsafe_allow_html=True,
 )
