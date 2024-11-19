ef8a5978adfc6f5fb56cd14b6bb49767541ec801
Avinash Kulkarni <40263379+kingKulkarni@users.noreply.github.com>
2024-11-19 21:33:28 +0530

    Update 02_📰_Evaluation.py

--- pages/02_📰_Evaluation.py
+++ pages/02_📰_Evaluation.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 import json
 import evaluate
+import pandas as pd
 
 validation_query_response = {
   0: {"query": "friendships between boy and girl",
@@ -101,50 +102,58 @@ def main():
            }
     }
 
-    query_string = validation_query_response_list[0]['query']
-    gold_response = validation_query_response_list[0]['gold_response']
-    gender, name, audience, setting, genre, topic, moral, word_count = query_string.split(',')
+    json_data = json.loads(validation_query_response_list)
+ 
+    for item in json_data:
+        query_string = item.get("query")
+        gold_response = item.get("gold_response")
     
-    st.write(f"{audience} {genre} {name} {moral} {setting} {word_count} ")
-
-    payload = {
-        "audience" : audience,
-        "story_type" : genre,
-        "main_character" : name,
-        "story_theme" : genre, 
-        "moral_lesson" : moral,
-        "setting" : setting, 
-        "word_count" : word_count,
-        "story_lang" : "English",
-        "api_Path" : "getStory"
-    }
+        gender, name, audience, setting, genre, topic, moral, word_count = query_string.split(',')
     
-    story_texts = fetch_story_data(payload)
-    # st.write(story_texts)
-    predicated_response = ' '.join(story_texts)
-
-    st.write(f"predicated_response - {predicated_response}")
+        st.write(f"{audience} {genre} {name} {moral} {setting} {word_count} ")
     
-    st.write("##### Evaluation Scores #####")
-    #st.write(f"gold_response - {gold_response}")
-
-    # gold_response = "In the enchanting Magical Kingdoms, where unicorns roamed and fairies danced, lived a young girl named Srila. Srilas days were filled with wonder and adventure, but there was one task she often neglected - brushing her teeth. One morning, as Srila was exploring the enchanted forest, she stumbled upon a curious creature. It was a tiny tooth fairy, with delicate wings and a sparkling wand. The tooth fairy fluttered around Srila, sprinkling a magical dust on her teeth. What is this? Srila asked, feeling a tingle in her mouth. This is a special tooth-cleaning dust, the fairy explained. It will keep your teeth strong and shiny, but you must remember to brush them every day. Srila promised to be more diligent about her dental hygiene. The tooth fairy smiled and vanished in a shower of glitter. From that day on, Srila made brushing her teeth a daily ritual. She used a soft-bristled brush and sweet-tasting toothpaste infused with fairy magic. Srila would sing songs and dance as she brushed, making it a fun and enjoyable activity. As Srilas teeth grew healthier and brighter, she noticed other changes too. Her breath stayed fresh, and she could enjoy all her favorite foods without any discomfort. The tooth fairy would visit occasionally, praising Srilas dedication and rewarding her with small treasures."
-    st.write(f"gold_response - {gold_response}")
-  
-    cal_rouge_scores= rouge_scores(predicated_response, gold_response)
-    # st.write(f"Calculated Rouge Scores - {cal_rouge_scores}")
-
-    st.write("##### Rouge Scores #####")
-    st.write(rouge_scores(predicated_response, gold_response))
+        payload = {
+            "audience" : audience,
+            "story_type" : genre,
+            "main_character" : name,
+            "story_theme" : genre, 
+            "moral_lesson" : moral,
+            "setting" : setting, 
+            "word_count" : word_count,
+            "story_lang" : "English",
+            "api_Path" : "getStory"
+        }
         
-    st.write("##### Bleu Scores #####")
-    st.write(bleu_scores(predicated_response, gold_response) )
+        story_texts = fetch_story_data(payload)
+        # st.write(story_texts)
+        predicated_response = ' '.join(story_texts)
     
-    st.write("##### Bert Scores #####")
-    st.write(bert_scores(predicated_response, gold_response))
+        st.write(f"predicated_response - {predicated_response}")
+        
+        st.write("##### Evaluation Scores #####")
+        #st.write(f"gold_response - {gold_response}")
     
-    # cal_bert_scores = bert_scores(predicated_response, gold_response)
-    # st.write(f"Calculated Bert  Scores - {bert_scores}")
+        st.write(f"gold_response - {gold_response}")
+      
+        cal_rouge_scores= rouge_scores(predicated_response, gold_response)
+        # st.write(f"Calculated Rouge Scores - {cal_rouge_scores}")
+    
+        st.write("##### Rouge Scores #####")
+        st.write(rouge_scores(predicated_response, gold_response))
+            
+        st.write("##### Bleu Scores #####")
+        st.write(bleu_scores(predicated_response, gold_response) )
+        
+        st.write("##### Bert Scores #####")
+        st.write(bert_scores(predicated_response, gold_response))
+        
+        # cal_bert_scores = bert_scores(predicated_response, gold_response)
+        # st.write(f"Calculated Bert  Scores - {bert_scores}")
+
+    # st.title("Display Excel File in Streamlit")
+    # df = pd.read_excel(uploaded_file)
+    # # Display the dataframe in Streamlit
+    # st.dataframe(df) 
 
 if __name__ == "__main__":
     main()
