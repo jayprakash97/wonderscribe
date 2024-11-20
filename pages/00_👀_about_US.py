2d6305ca572916e7618dffe801de0a507b62fe4e
Natsnet Demoz <25876006+Natsnet@users.noreply.github.com>
2024-11-19 21:21:29 -0800

    Update 00_👀_about_US.py

--- pages/00_👀_about_US.py
+++ pages/00_👀_about_US.py
@@ -64,8 +64,21 @@ st.markdown(
     unsafe_allow_html=True,
 )
  # +++++++
-                              
-st.image("pages/images/WonderScribeLogo.png", width=150)
+
+
+# Add the logo using HTML for precise positioning
+st.markdown(
+    """
+    <div class="top-left-logo">
+        <img src="pages/images/WonderScribeLogo.png" alt="WonderScribeLogo">
+    </div>
+    """,
+    unsafe_allow_html=True,
+)
+
+# +++++++
+
+#st.image("pages/images/WonderScribeLogo.png", width=150)
 st.title(" Welcome to WonderScribe")
 #st.write(
 
