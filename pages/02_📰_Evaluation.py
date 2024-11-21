cc2c61dc0cbbce14b0751d74c6327849824fb69c
Avinash Kulkarni <40263379+kingKulkarni@users.noreply.github.com>
2024-11-21 16:16:31 +0530

    Update 02_📰_Evaluation.py

--- pages/02_📰_Evaluation.py
+++ pages/02_📰_Evaluation.py
@@ -72,30 +72,6 @@ validation_query_response = {
       "gold_response": " "}
 }
 
-# Function to evaluate RAG system using ROUGE
-
-# def rouge_scores(predicated_response, gold_response):
-#   import evaluate
-#   rouge = evaluate.load('rouge')
-#   rouge_scores = rouge.compute(predictions=predicated_response, references=gold_response)
-#   return rouge_scores
-
-# def bleu_scores(predicated_response, gold_response):
-#   import evaluate
-#   bleu = evaluate.load('bleu')
-#   bleu_scores = bleu.compute(predictions=predicated_response, references=gold_response)
-#   return bleu_scores
-
-# def bert_scores(predicated_response, gold_response):
-#   import evaluate
-#   bertscore = evaluate.load('bertscore')
-#   bert_scores = bertscore.compute(predictions=predicated_response, references=gold_response, lang= 'en')
-#   bert_scores['precision'] = sum(bert_scores['precision']) / len(bert_scores['precision'])
-#   bert_scores['recall'] = sum(bert_scores['recall']) / len(bert_scores['recall'])
-#   bert_scores['f1'] = sum(bert_scores['f1']) / len(bert_scores['f1'])
-#   return bert_scores
-
-
 def rouge_scores(predicted_response, gold_response):
     try:
         rouge = evaluate.load('rouge')
