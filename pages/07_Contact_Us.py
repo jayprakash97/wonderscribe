7353b9c4af883e7cead28db8067842bb7997c9da
Jailynne Estevez <63680944+J3N21@users.noreply.github.com>
2024-11-24 11:27:32 -0500

    Update 07_Contact_Us.py

--- pages/07_Contact_Us.py
+++ pages/07_Contact_Us.py
@@ -61,11 +61,11 @@ st.markdown(
 
 # Contact form
 with st.form("form_key"):
-    Full_name = st.text_input("Enter your full name. (First Name, Middle Name, Last Name)")
-    Company = st.text_input("Enter your Company Name")
-    Country = st.text_input("Enter your Country Name")
-    State = st.text_input("Enter your State or Province Name")
-    email = st.text_input("Enter your email address")
-    phone = st.text_input("Enter your phone# (country code, city code, and local phone number)")
+    Full_name = st.text_input("Full Name (First Name, Last Name)")
+    Company = st.text_input("Company Name")
+    Country = st.text_input("Country Name")
+    State = st.text_input("State or Province Name")
+    email = st.text_input("Email Address")
+    phone = st.text_input("Phone Number (country code, city code, and local phone number)")
     question = st.text_area("Question or Comments")
     submit_btn = st.form_submit_button("Submit")
